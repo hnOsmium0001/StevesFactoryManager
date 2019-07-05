f69bc36ff5350b87cece356fa651096076db528f
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-07-04 21:25:46 -0700

    Remove unnecessary textures

--- src/main/java/vswe/stevesfactory/package-info.java
+++ src/main/java/vswe/stevesfactory/package-info.java
@@ -1,6 +1,6 @@
 /**
  * General Semantic Notes:
  * <p>
- * {@code transient} fields in a non-{@link java.io.Serializable} class means the field won't be saved into NBT.
+ * {@code transient} fields in a non-{@link java.io.Serializable Serializable} class means the field won't be saved into NBT.
  */
 package vswe.stevesfactory;
