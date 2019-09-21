d50abbba32e5021259946a8693eb9d49461d49f9
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-09-21 16:03:46 -0700

    Update language file, add text scaling to descriptions

--- src/main/java/vswe/stevesfactory/library/gui/widget/TextList.java
+++ src/main/java/vswe/stevesfactory/library/gui/widget/TextList.java
@@ -3,14 +3,12 @@ package vswe.stevesfactory.library.gui.widget;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import vswe.stevesfactory.library.gui.debug.RenderEventDispatcher;
-import vswe.stevesfactory.library.gui.layout.properties.HorizontalAlignment;
 import vswe.stevesfactory.library.gui.widget.mixin.LeafWidgetMixin;
-import vswe.stevesfactory.library.gui.RenderingHelper;
 
 import java.util.Collections;
 import java.util.List;
 
-import static vswe.stevesfactory.library.gui.RenderingHelper.*;
+import static vswe.stevesfactory.library.gui.RenderingHelper.fontHeight;
 
 public class TextList extends AbstractWidget implements LeafWidgetMixin {
 
@@ -18,7 +16,8 @@ public class TextList extends AbstractWidget implements LeafWidgetMixin {
     private List<String> textView;
     private boolean fitContents = false;
 
-    public HorizontalAlignment textAlignment = HorizontalAlignment.LEFT;
+    private int fontHeight = fontHeight();
+    private float scaleFactor = (float) fontHeight / fontHeight();
 
     public TextList(int width, int height, List<String> texts) {
         super(0, 0, width, height);
@@ -31,22 +30,15 @@ public class TextList extends AbstractWidget implements LeafWidgetMixin {
         RenderEventDispatcher.onPreRender(this, mouseX, mouseY);
         int x = getAbsoluteX() + 1;
         int y = getAbsoluteY() + 1;
-        int x2 = getAbsoluteXRight() - 1;
         GlStateManager.enableTexture();
+        GlStateManager.pushMatrix();
+        GlStateManager.translatef(x, y, 0F);
+        GlStateManager.scalef(scaleFactor, scaleFactor, 1F);
         for (String text : texts) {
-            switch (textAlignment) {
-                case LEFT:
-                    fontRenderer().drawString(text, x, y, 0x000000);
-                    break;
-                case CENTER:
-                    drawTextCenteredHorizontally(text, x, x2, y, 0x000000);
-                    break;
-                case RIGHT:
-                    fontRenderer().drawString(text, RenderingHelper.getXForAlignedRight(x2, textWidth(text)), y, 0x000000);
-                    break;
-            }
-            y += fontHeight();
+            fontRenderer().drawString(text, 0, 0, 0x000000);
+            GlStateManager.translatef(0F, fontHeight, 0F);
         }
+        GlStateManager.popMatrix();
         RenderEventDispatcher.onPostRender(this, mouseX, mouseY);
     }
 
@@ -79,6 +71,10 @@ public class TextList extends AbstractWidget implements LeafWidgetMixin {
         addLine(I18n.format(translationKey, args));
     }
 
+    public void addLineSplit(String text) {
+        addLineSplit(getWidth(), text);
+    }
+
     public void addLineSplit(int maxWidth, String text) {
         int end = fontRenderer().sizeStringToWidth(text, maxWidth);
         if (end >= text.length()) {
@@ -101,9 +97,18 @@ public class TextList extends AbstractWidget implements LeafWidgetMixin {
 
     private void tryExpand(String line) {
         if (fitContents) {
-            int w = minecraft().fontRenderer.getStringWidth(line);
+            int w = (int) (minecraft().fontRenderer.getStringWidth(line) * scaleFactor);
             setWidth(Math.max(getWidth(), 1 + w + 1));
-            setHeight(1 + (fontHeight() + 2) * texts.size() + 1);
+            setHeight(1 + (fontHeight + 2) * texts.size() + 1);
         }
+    }
+
+    public int getFontHeight() {
+        return fontHeight;
+    }
+
+    public void setFontHeight(int fontHeight) {
+        this.fontHeight = fontHeight;
+        this.scaleFactor = (float) fontHeight / fontHeight();
     }
 }
--- src/main/java/vswe/stevesfactory/ui/manager/menu/IntervalMenu.java
+++ src/main/java/vswe/stevesfactory/ui/manager/menu/IntervalMenu.java
@@ -1,12 +1,13 @@
 package vswe.stevesfactory.ui.manager.menu;
 
 import net.minecraft.client.resources.I18n;
-import vswe.stevesfactory.library.gui.widget.NumberField;
-import vswe.stevesfactory.library.gui.widget.TextField;
+import vswe.stevesfactory.library.gui.RenderingHelper;
+import vswe.stevesfactory.library.gui.widget.*;
 import vswe.stevesfactory.logic.procedure.TimedTriggerProcedure;
 import vswe.stevesfactory.ui.manager.editor.FlowComponent;
 import vswe.stevesfactory.ui.manager.editor.Menu;
-import vswe.stevesfactory.library.gui.RenderingHelper;
+
+import java.util.ArrayList;
 
 public class IntervalMenu extends Menu<TimedTriggerProcedure> {
 
@@ -20,7 +21,15 @@ public class IntervalMenu extends Menu<TimedTriggerProcedure> {
         int x = RenderingHelper.getXForAlignedCenter(0, getWidth(), interval.getWidth() + MARGIN_MIDDLE_UNIT_TEXT + fontRenderer().getStringWidth(getUnitText()));
         interval.setLocation(x, 50);
 
+        int desX = interval.getX();
+        TextList description = new TextList(getWidth() - x * 2, 0, new ArrayList<>());
+        description.setLocation(desX, HEADING_BOX.getPortionHeight() + 8);
+        description.setFitContents(true);
+        description.addLineSplit(getWidth() - 4 * 2, I18n.format("gui.sfm.Menu.Interval.Info"));
+        description.setFontHeight(8);
+
         addChildren(interval);
+        addChildren(description);
     }
 
     @Override
@@ -35,7 +44,13 @@ public class IntervalMenu extends Menu<TimedTriggerProcedure> {
     }
 
     @Override
+    public void render(int mouseX, int mouseY, float particleTicks) {
+        super.render(mouseX, mouseY, particleTicks);
+    }
+
+    @Override
     public void renderContents(int mouseX, int mouseY, float particleTicks) {
+        super.renderContents(mouseX, mouseY, particleTicks);
         RenderingHelper.drawTextCenteredVertically(
                 getUnitText(),
                 interval.getAbsoluteXRight() + MARGIN_MIDDLE_UNIT_TEXT,
